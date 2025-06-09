4d3d425bfd0118ab7e10bc727120e8e0a835522b
LavanyaC04 <chau.lavanya04@gmail.com>
2025-06-09 23:18:13 +0530

    Time: 1 ms (99.93%) | Memory: 62 MB (19.11%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -10,7 +10,7 @@ class Solution {
                     maxProfit = profit;
                 }
             }
-            if( buy > sell ){
+            else{
                 buy = sell;
             }
         }
