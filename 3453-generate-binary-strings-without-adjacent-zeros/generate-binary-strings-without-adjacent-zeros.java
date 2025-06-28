cb3899084e654c9ea4e90e51233a4efba9febbfe
Lavanya Chaudhari <chau.lavanya04@gmail.com>
2025-06-28 21:16:22 +0530

    Time: 3 ms (74.37%) | Memory: 46.3 MB (13.88%) - LeetSync

--- 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.java
+++ 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.java
@@ -1,23 +1,17 @@
 class Solution {
-     List<String>validStrings= new ArrayList<>();
     public List<String> validStrings(int n) {
-
-        generateStrings(n,"",'\0'); //pass null character which indicates no previous character up until now.
-       return validStrings; 
+        List<String> validStrings = new ArrayList<>();
+        generateStrings(n, "", '\0', validStrings);
+        return validStrings;
     }
-    public  void generateStrings(int n, String str, char prev)
-    {
-        if(n==0) // base condition is met
-        {
+    public void generateStrings(int n, String str, char prev, List<String> validStrings) {
+        if (n == 0) {
             validStrings.add(str);
             return;
         }
-        // Add '0' only if the previous character is not zero
-        if(prev!='0')
-        {
-           generateStrings(n-1, str + "0",'0'); 
+        if (prev != '0') {
+            generateStrings(n - 1, str + "0", '0', validStrings);
         }
-       // 1 can be added always anywhere
-        generateStrings(n-1, str + "1",'1');
+        generateStrings(n - 1, str + "1", '1', validStrings);
     }
 }
