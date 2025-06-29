1b43c5ea9992de5a97f771283e21c01d11638293
Lavanya Chaudhari <chau.lavanya04@gmail.com>
2025-06-29 23:31:57 +0530

    Time: 18 ms (62.74%) | Memory: 43.8 MB (78.70%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,33 +1,35 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        int n = nums.length; 
-Set<List<Integer>> st = new HashSet<>();
-
-        // checking all possible quadruplets:
-        for (int i = 0; i < n; i++) {
+        int n = nums.length; List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < n; i++){
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
             for (int j = i + 1; j < n; j++) {
-                Set<Long> hashset = new HashSet<>();
-                for (int k = j + 1; k < n; k++) {
-                    // taking bigger data type
-                    // to avoid integer overflow:
-                    long sum = nums[i] + nums[j];
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                int k = j + 1;
+                int l = n - 1;
+                while (k < l) {
+                    long sum = nums[i];
+                    sum += nums[j];
                     sum += nums[k];
-                    long fourth = target - sum;
-                    if (hashset.contains(fourth)) {
+                    sum += nums[l];
+                    if (sum == target) {
                         List<Integer> temp = new ArrayList<>();
                         temp.add(nums[i]);
                         temp.add(nums[j]);
                         temp.add(nums[k]);
-                        temp.add((int) fourth);
-                        temp.sort(Integer::compareTo);
-                        st.add(temp);
-                    }
-                    // put the kth element into the hashset:
-                    hashset.add((long) nums[k]);
+                        temp.add(nums[l]);
+                        ans.add(temp);
+                        k++;
+                        l--;
+                        while (k < l && nums[k] == nums[k - 1]) k++;
+                        while (k < l && nums[l] == nums[l + 1]) l--;
+                    } else if (sum < target) k++;
+                    else l--;
                 }
             }
         }
-        List<List<Integer>> ans = new ArrayList<>(st);
+
         return ans;
     }
 }
