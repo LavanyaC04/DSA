e1d05f82ce32ef9c42ca0d5417b2439d3da3a319
Lavanya Chaudhari <chau.lavanya04@gmail.com>
2025-06-29 14:54:48 +0530

    Time: 3 ms (75.78%) | Memory: 50.4 MB (53.51%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -2,21 +2,43 @@ class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int n = nums.length; 
         List<Integer> ls = new ArrayList<>();
+        int cnt1 = 0;
+        int cnt2 = 0;
+        int el1 = 0;
+        int el2 = 0;
         for(int i = 0 ; i < n ; i++){
-            if(ls.size() == 0 || ls.get(0) != nums[i]){
-                int cnt = 0;
-                for(int j = 0 ; j < n ; j++){
-                    if(nums[j] == nums[i]){
-                        cnt++;
-                    }
-                }
-                if(cnt > (n/3)){
-                    ls.add(nums[i]);
-                }
-                if(ls.size() == 2) break;
+            if(cnt1 == 0 && nums[i] != el2){
+                el1 = nums[i];
+                cnt1 = 1;
             }
-            
+            else if(cnt2 == 0 && nums[i] != el1){
+                el2 = nums[i];
+                cnt2 = 1;
+            }
+            else if(nums[i] == el1){
+                cnt1++;
+            }
+            else if(nums[i] == el2){
+                cnt2++;
+            }
+            else{
+                cnt1--;
+                cnt2--;
+            }
+
         }
+        cnt1 = 0 ;
+         cnt2 = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(el1 == nums[i]){
+                cnt1++;
+            }
+            else if(el2 == nums[i]){
+                cnt2++;
+            }
+        }
+        if(cnt1 > (n/3)) ls.add(el1);
+        if(cnt2 > (n/3)) ls.add(el2);
         return ls;
     }
 }
