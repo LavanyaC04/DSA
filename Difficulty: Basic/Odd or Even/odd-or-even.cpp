062eae97b95515407e17e7b3150b20948c584569
Lavanya Chaudhari <chau.lavanya04@gmail.com>
2025-07-01 05:27:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Odd or Even/odd-or-even.cpp
+++ Difficulty: Basic/Odd or Even/odd-or-even.cpp
@@ -2,7 +2,7 @@ class Solution {
   public:
     bool isEven(int n) {
         // code here
-        if ((n ^ 1) == (n + 1))
+        if ((n & 1) == 0)
             return true;
         else
             return false;
