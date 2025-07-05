82a21b6f404303f89544cdc40c8f4bb95e5a68a7
Lavanya Chaudhari <chau.lavanya04@gmail.com>
2025-07-05 05:42:52 +0530

    Time: 6 ms (30.50%) | Memory: 45.8 MB (45.72%) - LeetSync

--- 260-single-number-iii/single-number-iii.java
+++ 260-single-number-iii/single-number-iii.java
@@ -1,20 +1,24 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
         int n = nums.length;
-        int xor = 0;
-        for (int i = 0 ; i < n ; i++) {
-            xor = xor ^ nums[i];
-        }
-        int rightmost = xor & -xor;
-        int bucket1 = 0, bucket2 = 0;
-        for (int i = 0 ; i < n ; i++) {
-            if ((nums[i] & rightmost) != 0) {
-                bucket1 = bucket1 ^ nums[i];
+        HashMap<Integer, Integer> mpp = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (mpp.containsKey(nums[i])) {
+                mpp.put(nums[i], mpp.get(nums[i]) + 1);
             } else {
-                bucket2 = bucket2 ^ nums[i];
+                mpp.put(nums[i], 1);
             }
         }
-        return new int[]{bucket1, bucket2};
-
+        ArrayList<Integer> ansList = new ArrayList<>();
+        for (HashMap.Entry<Integer, Integer> entry : mpp.entrySet()) {
+            if (entry.getValue() == 1) {
+                ansList.add(entry.getKey());
+            }
+        }
+        int[] ans = new int[ansList.size()];
+        for (int i = 0; i < ansList.size(); i++) {
+            ans[i] = ansList.get(i);
+        }
+        return ans;
     }
 }
