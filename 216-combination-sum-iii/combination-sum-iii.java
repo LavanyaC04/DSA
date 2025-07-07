549f8b71f1b41c77a0cdf409000acbbc0ae0b8b9
Lavanya Chaudhari <chau.lavanya04@gmail.com>
2025-07-08 00:05:34 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (78.31%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.java
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -1,7 +1,3 @@
-/* This question is the continuation of Combination Sum 1 & 2
-The only difference is that, instead of giving us an array 
-We are asked to print all possible combinations using numbers 1 to 9 (both inclusive)
-*/
 
 class Solution{
     public void help(int i, int k, int sumTillNow, int n, List<Integer> subSet, List<List<Integer>> ans) {
