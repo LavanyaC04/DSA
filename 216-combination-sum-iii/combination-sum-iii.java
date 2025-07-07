6ba21c35c43bf0d663ae6b02a95f9f203048619e
Lavanya Chaudhari <chau.lavanya04@gmail.com>
2025-07-08 00:05:19 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (42.29%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.java
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -3,16 +3,12 @@ The only difference is that, instead of giving us an array
 We are asked to print all possible combinations using numbers 1 to 9 (both inclusive)
 */
 
-class Solution
-{
-    public void help(int i, int k, int sumTillNow, int n, List<Integer> subSet, List<List<Integer>> ans) 
-    {
+class Solution{
+    public void help(int i, int k, int sumTillNow, int n, List<Integer> subSet, List<List<Integer>> ans) {
         if(sumTillNow > n) 
             return ;
-        if(k == 0) 
-        {
-            if(sumTillNow == n) 
-            {
+        if(k == 0) {
+            if(sumTillNow == n) {
                 ans.add(new ArrayList(subSet)); 
             }
             return;
@@ -26,8 +22,7 @@ class Solution
         subSet.remove(subSet.size()-1) ;
         help(i + 1, k, sumTillNow, n, subSet, ans); 
     }
-    public List<List<Integer>> combinationSum3(int k, int n) 
-    {
+    public List<List<Integer>> combinationSum3(int k, int n) {
         List<Integer> subSet = new ArrayList<>();
         List<List<Integer>> ans = new ArrayList<>();
         help(1, k, 0, n, subSet, ans) ;
