7006c0ee1d7271c44129204bca98b09189bd4af4
Lavanya Chaudhari <chau.lavanya04@gmail.com>
2025-07-13 21:37:49 +0530

    Time: 3 ms (91.35%) | Memory: 43.4 MB (63.03%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,12 +3,15 @@ class Solution {
         if(s.length() != t.length()){
             return false;
         }
-        char[] sArr = s.toCharArray();
-        char[] tArr = t.toCharArray();
-        Arrays.sort(sArr);
-        Arrays.sort(tArr);
+        int[] freq = new int[26];
         for(int i = 0 ; i < s.length() ; i++){
-            if(sArr[i] != tArr[i]){
+            freq[s.charAt(i) - 'a']++;
+        }
+        for(int i = 0 ; i < t.length() ; i++){
+            freq[t.charAt(i) - 'a']--;
+        }
+        for(int i = 0 ; i < 26 ; i++){
+            if(freq[i] != 0){
                 return false;
             }
         }
