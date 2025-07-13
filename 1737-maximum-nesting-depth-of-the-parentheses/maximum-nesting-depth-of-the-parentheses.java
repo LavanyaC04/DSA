7cb5b11874a5145af44cc92c081f72ed82f9b28e
Lavanya Chaudhari <chau.lavanya04@gmail.com>
2025-07-14 05:06:49 +0530

    Time: 1 ms (26.52%) | Memory: 41.8 MB (26.49%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.java
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.java
@@ -6,11 +6,12 @@ class Solution {
             char ch = s.charAt(i);
             if(ch == '('){
                 stack.push(i);
-            }
-            else if(ch == ')'){
                 if(count < stack.size()){
                     count = stack.size();
                 }
+            }
+            else if(ch == ')'){
+                
                 stack.pop();
             }
         }
