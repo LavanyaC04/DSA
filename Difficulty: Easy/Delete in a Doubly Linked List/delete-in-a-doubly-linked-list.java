0d051952f7a256dbb2ab3b2347c533239f2da6a2
Lavanya Chaudhari <chau.lavanya04@gmail.com>
2025-07-31 22:15:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Delete in a Doubly Linked List/delete-in-a-doubly-linked-list.java
@@ -0,0 +1,36 @@
+/*
+
+Definition for doubly Link List Node
+class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int val) {
+        data = val;
+        next = null;
+        prev = null;
+    }
+}
+*/
+class Solution {
+    public Node deleteNode(Node head, int x) {
+        // code here
+        if (head == null)
+            return head;
+        Node curr = head;
+        for (int i = 1; curr != null && i < x; ++i) {
+            curr = curr.next;
+        }
+        if (curr == null)
+            return head;
+        if (curr.prev != null)
+            curr.prev.next = curr.next;
+        if (curr.next != null)
+            curr.next.prev = curr.prev;
+        if (head == curr)
+            head = curr.next;
+        curr = null;
+        return head;
+    }
+}
